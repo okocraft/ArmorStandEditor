9b5afe9b4abefb9bceda2d309ca1ac9c4b76d57b
Siroshun09 <siro.shun8@gmail.com>
2021-03-11 05:23:51 +0900

    refactor(plugin): add KEY_PREFIX

--- src/main/java/net/okocraft/armorstandeditor/lang/Components.java
+++ src/main/java/net/okocraft/armorstandeditor/lang/Components.java
@@ -5,22 +5,24 @@ import net.kyori.adventure.text.format.NamedTextColor;
 
 public final class Components {
 
-    private Components() {
-        throw new UnsupportedOperationException();
-    }
+    public static final String KEY_PREFIX = "armorstandeditor.";
 
     public static final Component PREFIX =
             Component.translatable("armorstandeditor.prefix").color(NamedTextColor.GRAY);
 
     public static final Component DISABLE_MANIPULATION_1 =
-            PREFIX.append(Component.translatable("armorstandeditor.disable-manipulation.line-1"));
+            PREFIX.append(Component.translatable(KEY_PREFIX + "disable-manipulation.line-1"));
 
     public static final Component DISABLE_MANIPULATION_2 =
-            PREFIX.append(Component.translatable("armorstandeditor.disable-manipulation.line-2"));
+            PREFIX.append(Component.translatable(KEY_PREFIX + "disable-manipulation.line-2"));
 
     public static final Component EQUIPMENT_MENU_TITLE =
-            Component.translatable("armorstandeditor.menu.equipment.title").color(NamedTextColor.BLACK);
+            Component.translatable(KEY_PREFIX + "menu.equipment.title").color(NamedTextColor.BLACK);
 
     public static final Component SELECTION_MENU_TITLE =
-            Component.translatable("armorstandeditor.menu.selection.title").color(NamedTextColor.BLACK);
+            Component.translatable(KEY_PREFIX + "menu.selection.title").color(NamedTextColor.BLACK);
+
+    private Components() {
+        throw new UnsupportedOperationException();
+    }
 }
