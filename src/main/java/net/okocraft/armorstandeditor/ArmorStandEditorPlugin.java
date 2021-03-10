f18c28935bb4718564d3de343baf9d5df7b93c08
Siroshun09 <siro.shun8@gmail.com>
2021-03-10 23:30:19 +0900

    feat(plugin): add ArmorStandEditorPlugin#runTask

--- src/main/java/net/okocraft/armorstandeditor/ArmorStandEditorPlugin.java
+++ src/main/java/net/okocraft/armorstandeditor/ArmorStandEditorPlugin.java
@@ -1,6 +1,17 @@
 package net.okocraft.armorstandeditor;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 public final class ArmorStandEditorPlugin extends JavaPlugin {
+
+    private static Plugin PLUGIN;
+
+    public static void runTask(@NotNull Runnable runnable) {
+        if (PLUGIN != null && PLUGIN.isEnabled()) {
+            Bukkit.getScheduler().runTaskLater(PLUGIN, runnable, 1);
+        }
+    }
 }
