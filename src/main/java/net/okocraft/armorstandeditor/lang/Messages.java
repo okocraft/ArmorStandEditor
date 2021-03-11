d80f2b3725ede08f84902aafb233af89ecb29b1c
Siroshun09 <siro.shun8@gmail.com>
2021-03-11 12:37:38 +0900

    feat(command): /ase equipment を実装する

--- src/main/java/net/okocraft/armorstandeditor/lang/Messages.java
+++ src/main/java/net/okocraft/armorstandeditor/lang/Messages.java
@@ -15,6 +15,12 @@ public final class Messages {
     public static final Component COMMAND_NO_PERMISSION =
             Components.PREFIX.append(Component.translatable(KEY_PREFIX + "command.no-permission", RED));
 
+    public static final Component COMMAND_ONLY_PLAYER =
+            Components.PREFIX.append(Component.translatable(KEY_PREFIX + "command.only-player", RED));
+
+    public static final Component COMMAND_ARMOR_STAND_NOT_FOUND =
+            Components.PREFIX.append(Component.translatable(KEY_PREFIX + "command.armor-stand-not-found", RED));
+
     public static final Component COMMAND_RELOAD_SUCCESS =
             Components.PREFIX.append(Component.translatable(KEY_PREFIX + "command.reload.success", GRAY));
 
