36815228475a592bb951f976a0e8f869330e8955
Siroshun09 <siro.shun8@gmail.com>
2021-03-11 12:27:37 +0900

    feat(command): /ase reload に使われるメッセージを追加する

--- src/main/java/net/okocraft/armorstandeditor/lang/Messages.java
+++ src/main/java/net/okocraft/armorstandeditor/lang/Messages.java
@@ -2,6 +2,7 @@ package net.okocraft.armorstandeditor.lang;
 
 import net.kyori.adventure.text.Component;
 
+import static net.kyori.adventure.text.format.NamedTextColor.GRAY;
 import static net.kyori.adventure.text.format.NamedTextColor.RED;
 import static net.okocraft.armorstandeditor.lang.Components.KEY_PREFIX;
 
@@ -14,5 +15,9 @@ public final class Messages {
     public static final Component COMMAND_NO_PERMISSION =
             Components.PREFIX.append(Component.translatable(KEY_PREFIX + "command.no-permission", RED));
 
+    public static final Component COMMAND_RELOAD_SUCCESS =
+            Components.PREFIX.append(Component.translatable(KEY_PREFIX + "command.reload.success", GRAY));
 
+    public static final Component COMMAND_RELOAD_FAILURE =
+            Components.PREFIX.append(Component.translatable(KEY_PREFIX + "command.reload.failure", GRAY));
 }
