691bfa935201b5f0f97abaf90295da60f465e75c
Siroshun09 <siro.shun8@gmail.com>
2023-12-21 00:41:17 +0900

    fix: FoliaSyncTeleporter in 1.20.4

--- src/main/java/net/okocraft/armorstandeditor/util/FoliaSyncTeleporter.java
+++ src/main/java/net/okocraft/armorstandeditor/util/FoliaSyncTeleporter.java
@@ -1,8 +1,10 @@
 package net.okocraft.armorstandeditor.util;
 
+import net.okocraft.armorstandeditor.ArmorStandEditorPlugin;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Method;
@@ -10,8 +12,10 @@ import java.lang.reflect.Method;
 public final class FoliaSyncTeleporter {
 
     private static volatile boolean initialized = false;
+    private static Method toVec3D;
+    private static Object vec3DZero;
     private static Method getHandle;
-    private static Method moveTo;
+    private static Method teleportSyncSameRegion;
 
     public static void teleport(@NotNull Entity entity, @NotNull Location loc) {
         if (!initialized) {
@@ -19,7 +23,8 @@ public final class FoliaSyncTeleporter {
                 if (!initialized) { // prevent double initializing
                     try {
                         init(entity);
-                    } catch (Exception ignored) {
+                    } catch (Exception e) {
+                        logError("Could not initialize reflections.", e);
                     } finally {
                         initialized = true;
                     }
@@ -27,10 +32,12 @@ public final class FoliaSyncTeleporter {
             }
         }
 
-        if (moveTo != null) {
+        if (teleportSyncSameRegion != null) {
             try {
-                moveTo.invoke(getHandle.invoke(entity), loc.x(), loc.y(), loc.z());
-            } catch (Exception ignored) {
+                var handle = getHandle.invoke(entity);
+                teleportSyncSameRegion.invoke(handle, toVec3D.invoke(null, loc), null, null, vec3DZero);
+            } catch (Exception e) {
+                logError("Could not invoke methods", e);
             }
         } else {
             entity.teleportAsync(loc).join(); // very unstable...
@@ -38,27 +45,23 @@ public final class FoliaSyncTeleporter {
     }
 
     private static void init(@NotNull Entity entity) throws Exception {
+        // CraftLocation#toVec3D
+        var craftLocation = Class.forName(Bukkit.getServer().getClass().getPackage().getName() + ".util.CraftLocation");
+        toVec3D = craftLocation.getDeclaredMethod("toVec3D", Location.class);
+        vec3DZero = toVec3D.invoke(null, new Location(null, 0, 0, 0));
+
         // CraftEntity#getHandle
         getHandle = entity.getClass().getMethod("getHandle");
         getHandle.setAccessible(true);
 
-        var handle = getHandle.invoke(entity);
+        var entityClass = getHandle.invoke(entity).getClass().getSuperclass().getSuperclass(); // ArmorStand -> LivingEntity -> Entity
 
-        // Entity#moveTo(double x, double y, double z)
-        moveTo = handle.getClass().getMethod(getObfNameOfMoveTo(), double.class, double.class, double.class);
-        moveTo.setAccessible(true);
+        // Entity#teleportSyncSameRegion(Vec3 pos, Float yaw, Float pitch, Vec3 speedDirectionUpdate)
+        teleportSyncSameRegion = entityClass.getDeclaredMethod("teleportSyncSameRegion", vec3DZero.getClass(), Float.class, Float.class, vec3DZero.getClass());
+        teleportSyncSameRegion.setAccessible(true);
     }
 
-    @SuppressWarnings("SwitchStatementWithTooFewBranches")
-    private static String getObfNameOfMoveTo() {
-        /*
-          Steps to get obfuscation name:
-            1. Get the server obfuscation maps (ex: Go https://minecraft.fandom.com/wiki/Java_Edition_%VERSION%, and open the "Server" link in "Obfuscation maps")
-            2. Looking for "void moveTo(double,double,double) ->" in the page
-            3. The name after "->" is the obfuscated method name
-        */
-        return switch (Bukkit.getMinecraftVersion()) {
-            default /* case "1.19.4", "1.20", "1.20.1" */ -> "d"; // Folia is only available in 1.19.4+
-        };
+    private static void logError(@NotNull String msg, @NotNull Throwable e) {
+        JavaPlugin.getPlugin(ArmorStandEditorPlugin.class).getSLF4JLogger().error(msg, e);
     }
 }
