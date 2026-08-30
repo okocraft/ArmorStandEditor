110548eef09e31dcb0cec9447bc02f02dc9e7890
しろしゅん <39247022+Siroshun09@users.noreply.github.com>
2026-08-30 19:51:38 +0900

    fix: open equipment menus through provider

--- src/main/java/net/okocraft/armorstandeditor/menu/EquipmentMenuProvider.java
+++ src/main/java/net/okocraft/armorstandeditor/menu/EquipmentMenuProvider.java
@@ -1,6 +1,7 @@
 package net.okocraft.armorstandeditor.menu;
 
 import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.HumanEntity;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -17,8 +18,8 @@ public final class EquipmentMenuProvider {
         throw new UnsupportedOperationException();
     }
 
-    public static @NotNull EquipmentMenu getMenu(@NotNull ArmorStand armorStand) {
-        return MENU_MAP.computeIfAbsent(armorStand.getUniqueId(), u -> new EquipmentMenu(armorStand));
+    public static boolean openMenu(@NotNull ArmorStand armorStand, @NotNull HumanEntity viewer) {
+        return MENU_MAP.computeIfAbsent(armorStand.getUniqueId(), u -> new EquipmentMenu(armorStand)).open(viewer);
     }
 
     public static @Nullable EquipmentMenu getMenuOrNull(@NotNull ArmorStand armorStand) {
