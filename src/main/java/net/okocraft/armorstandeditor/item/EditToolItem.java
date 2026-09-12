26db69a429ca466696c3942af104006e8eedbedb
しろしゅん <39247022+Siroshun09@users.noreply.github.com>
2026-09-12 23:39:38 +0900

    refactor: create edit tool item from config

--- src/main/java/net/okocraft/armorstandeditor/item/EditToolItem.java
+++ src/main/java/net/okocraft/armorstandeditor/item/EditToolItem.java
@@ -3,19 +3,38 @@ package net.okocraft.armorstandeditor.item;
 import io.papermc.paper.datacomponent.DataComponentTypes;
 import io.papermc.paper.datacomponent.item.ItemLore;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class EditToolItem {
 
     private static final NamespacedKey EDIT_ITEM_KEY = new NamespacedKey("armorstandeditor", "edititem");
     private static final byte EDIT_ITEM_VALUE = 1;
+
+    public static @NotNull EditToolItem createFromConfig(@NotNull ConfigurationSection config) {
+        boolean allowNormalFlint = config.getBoolean("tool.allow-normal-flint");
+        String displayName = config.getString("tool.display-name");
+
+        var serializer = LegacyComponentSerializer.legacyAmpersand();
+        var lore = config.getStringList("tool.lore").stream()
+            .map(serializer::deserialize)
+            .collect(Collectors.toUnmodifiableList());
+
+        return new EditToolItem(
+            allowNormalFlint,
+            displayName != null ? serializer.deserialize(displayName) : null,
+            lore
+        );
+    }
 
     private final boolean allowNormalFlint;
     private final ItemStack item;
